38a91f40a3f786f7c577676f97b7ee1e1fd6a68f
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-10-24 23:50:09 +0300

    Object HomeWork

--- js/script.js
+++ js/script.js
@@ -1,90 +1,14 @@
-//1 +
-// let userNum = prompt("Введите свое число", "");
+let userInfo = {
+    name: "Vasya",
+    age: 30,
+};
 
-// if (userNum > 0) {
-//     alert("Положительное")
-// } else {
-//     alert("Отрицательное");
-// }
+console.log(userInfo);
 
-//===============
+userInfo.name = "Lena";
 
-//2 +
-// let userString = "Hello world!";
+console.log(userInfo.name);
 
-// console.log(userString.length);
+delete userInfo.name;
 
-//===============
-
-//3 +
-//1 task
-// for(i=0; i<6; i++) {
-//     console.log(i);
-// };
-// let i = 0;
-// while(i < 6) {
-//     console.log(i);
-//     i++;
-// }
-
-//2 task
-// let num = 8;
-// while(num) {
-//     console.log(num);
-//     num--;
-// }
-
-//3 task
-// let num = 0;
-// while(num < 3) {
-//     console.log(`Number: ${num++}`);
-// }
-
-//4 task
-
-// firtFor:for (let num = 0; num < 2; num++) {
-//     for(let size = 0; size < 3; size++) {
-//         if(size == 1) {
-//             break firtFor;
-//         }
-//         console.log(size);
-//     }
-// }
-
-//===============
-
-//4 +
-
-// let userNum = prompt("Введите свое число", "");
-
-// if (userNum % 2 === 0) {
-//     alert("Положительное")
-// } else {
-//     alert("Отрицательное")
-// }
-
-//===============
-
-//5 +
-
-//1 task
-// function showName() {
-//     console.log("Вася!");
-// }
-// setTimeout(showName, 0);
-// console.log("Коля!");
-
-//2 task
-// showMessage();
-// function showMessage() {
-//     console.log("Hello world!");
-// }
-
-//3 task
-// 'use strict'
-// if (2 > 1) {
-//     function showMessage() {
-//         console.log("Message");
-//     }
-//     showMessage();
-// }
+console.log(userInfo);
