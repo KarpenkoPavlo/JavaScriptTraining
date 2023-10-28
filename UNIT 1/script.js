f227a430b9dec68a7b33700216e6f270713d577b
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-10-28 22:45:55 +0300

    Completed UNIT 1

---
+++ UNIT 1/script.js
@@ -0,0 +1,81 @@
+//task 1
+console.log("Pavel");
+
+//task 2
+console.log(10);
+
+//Різниця у виведені числа від рядка: колір(рядок = чорний\число = фіолетовий)
+
+//task 3
+console.log("Вітаємо" + " на курсі по" + " JavaScript");
+
+//task 4
+// alert(2023);
+
+//task 5
+// alert(2023 - 23);
+
+//task 6
+document.getElementById("one").innerHTML = "Hello world";
+
+//task 7
+document.getElementById("two").innerHTML = (12 * 12);
+
+//task 8
+document.querySelector(".one").innerHTML = "Hello world";
+
+//task 9
+document.querySelector("span").innerHTML = "world";
+
+//task 10
+document.querySelector(".three").innerHTML = "<h3>h3</h3>";
+
+//task 11
+document.querySelector(".four").innerHTML += "<h4>h4</h4> <p>p</p>"
+
+//task 12
+let a = document.querySelector(".five").innerHTML = 3.1415;
+
+//task 13
+let div7 = document.querySelector(".seven").innerHTML = '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">'
+
+//task 14
+let z1 = 6;
+let z2 = 3;
+
+document.querySelector(".plus").innerHTML = (z1 * z2);
+
+//task 15
+y1 = 6;
+y2 = 3;
+
+document.querySelector(".plusTwo").innerHTML = (y1 / y2);
+
+//task 16
+let x1 = "Hello";
+let x2 = 5;
+
+document.querySelector(".plusThree").innerHTML = (x1 + x2);
+
+//task17
+let d1 = document.querySelector(".test-1");
+
+console.log(d1);
+
+//task 18
+let d2 = document.querySelector(".test-2");
+console.log(d2);
+
+d2 = 5;
+console.log(d2);
+
+
+//task 19
+let divS3 = document.querySelector(".s3");
+console.log(divS3);
+
+divS3 = document.querySelector(".s4");
+console.log(divS3);
+
+//task 20
+// document.querySelector("body").innerHTML = "";
