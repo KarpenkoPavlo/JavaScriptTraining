9b42b071986e55bc2d0819d7f71adbcf83b21f8a
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-11-19 14:45:29 +0200

    Completed UNIT 9. 12 - 20 tasks

--- UNIT 9/script.js
+++ UNIT 9/script.js
@@ -105,3 +105,84 @@ function f11() {
 }
 
 document.querySelector(".b11").onclick = f11;
+
+//task 12
+document.querySelector('.b12').onclick = function f12() {
+    let newDiv = document.createElement('div');
+    newDiv.textContent = '12';
+    newDiv.classList.add('bg12');
+    document.querySelector('.o12').append(newDiv);
+}
+
+//task 13
+document.querySelector('.b13').onclick = function f13() {
+    let newDiv = document.createElement('div');
+    newDiv.textContent = 'pushMe';
+    newDiv.classList.add('bg13');
+    newDiv.onclick = function f131() {
+        document.querySelector('.o13').append(newDiv);
+    }
+    document.querySelector('.o13').append(newDiv);
+}
+
+//task 14
+document.querySelector('.b14').onclick = function f14() {
+    let newDiv = document.createElement('div');
+    newDiv.textContent = '14';
+    newDiv.classList.add('bg14');
+    document.querySelector('.o14').prepend(newDiv);
+}
+
+//task 15
+function f15() {
+    let newDiv = document.createElement('div');
+    newDiv.textContent = '15';
+    newDiv.classList.add('bg15');
+    document.querySelector('.o15').before(newDiv);
+}
+
+document.querySelector('.b15').onclick = f15;
+
+//task 16
+function f16() {
+    let newDiv = document.createElement('div');
+    newDiv.textContent = '16';
+    newDiv.classList.add('bg16');
+    document.querySelector('.o16').after(newDiv);
+}
+
+document.querySelector('.b16').onclick = f16;
+
+//task 17
+function f17() {
+    let newDiv = document.createElement('div');
+    newDiv.textContent = '17';
+    newDiv.classList.add('bg17');
+    document.querySelector('.o17').replaceWith(newDiv);
+}
+
+document.querySelector('.b17').onclick = f17;
+
+//task 18
+document.querySelector('.b18').onclick = function f18() {
+    let getP = document.querySelector('.p18');
+    document.querySelector('.o18').innerHTML = getP.getAttribute('data-b');
+}
+
+//task 19
+document.querySelector('.b19').onclick = function f19() {
+    let getP = document.querySelectorAll('.p19');
+    let o19 = document.querySelector('.o19');
+    o19.innerHTML = '';
+    let i = 0;
+    while(i<getP.length) {
+        o19.innerHTML += getP[i].getAttribute('data-b') + " ";
+        i++;
+    }
+}
+
+//task 20
+document.querySelector('.b20').onclick = function f20() {
+    let o20 = document.querySelector('.o20');
+    o20.setAttribute('title', 'go');
+}
