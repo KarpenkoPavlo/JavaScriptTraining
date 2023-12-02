09a1ecafe5088f1dea468d8c2bfedde1741dc02b
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-12-02 15:16:10 +0200

    Completed 9 - 14 tasks in UNIT 13

--- UNIT 13/script.js
+++ UNIT 13/script.js
@@ -146,7 +146,7 @@ function f8() {
 
 document.querySelector('.b8').onclick = f8;
 
-//task 9-
+//task 9
 let a9 = {
     "b": 17,
     "e": 22,
@@ -157,13 +157,144 @@ let a9 = {
 
 function f9() {
     let i9 = document.querySelector('.i9').value;
-    let o9 = document.querySelector('.o9');
+    let keyArr = [];
 
-    if(a9[i9] == undefined) {
-        o9.innerHTML = '';
-    } else {
-        o9.innerHTML += a9.i9;
+    for(key in a9) {
+        if(a9[key] == i9) {
+            keyArr.push(key);
+        }
     }
+    document.querySelector('.o9').innerHTML = keyArr.join(' ');
 }
 
 document.querySelector('.b9').onclick = f9;
+
+//task 10
+function f10(arr, val) {
+
+    for(key in arr) {
+        if(arr[key] == val) {
+            return true;
+        }
+    }
+    return false;
+}
+
+document.querySelector('.b10').onclick = () => {
+    let a10 = {
+        "k": 22,
+        "d": 54,
+        "m": 22,
+    }
+    document.querySelector('.o10').innerHTML = f10(a10, 22);
+};
+
+//task 11 Використовуємо функцію f5
+// function f5(arr, block) {
+//     let out = '';
+//     for(key in arr) {
+//         out += `${key} : ${arr[key]} <br>`;
+//     }
+//     document.querySelector(block).innerHTML = out;
+// }
+
+let a11 = {
+    "b": 17,
+    "e": 22,
+    "j": 17,
+    "k": 22,
+    "d": 54
+};
+
+function f11() {
+    let i11 = document.querySelector('.i11').value;
+
+    for(key in a11) {
+        if(key == i11) {
+            delete a11[key];
+        }
+    }
+    f5(a11, '.o11');
+}
+
+document.querySelector('.b11').onclick = f11;
+
+//task 12
+let a12 = {
+    "b": 17,
+    "e": 22,
+    "j": 17,
+    "k": 22,
+    "d": 17,
+};
+
+function f12() {
+    let i12 = document.querySelector('.i12').value;
+
+    for(key in a12) {
+        if(a12[key] == i12) {
+            delete a12[key];
+        }
+    }
+    f5(a12, '.o12');
+}
+
+document.querySelector('.b12').onclick = f12;
+
+//task 13
+let a13 = {
+    'prim': 'hello',
+    'one': 4,
+    'testt': 'vodoley',
+    'ivan': 6
+};
+
+function f13() {
+    let sum = 0;
+    for(key in a13) {
+        if(typeof(a13[key]) == "number") {
+            sum = sum + a13[key];
+        }
+    }
+    document.querySelector('.o13').innerHTML = sum;
+}
+
+document.querySelector('.b13').onclick = f13;
+
+//task 14
+let a14 = {
+    'prim': [1, 2, 23],
+    'one': [3, 4, 5],
+    'testt': [6, 7, 8],
+    'ivan': [9, 10]
+};
+
+function f14() {
+    let out = '';
+    for(key in a14) {
+        out += a14[key][0] + ' ';
+    }
+    document.querySelector('.o14').innerHTML = out;
+}
+
+document.querySelector('.b14').onclick = f14;
+
+//task 15 -
+let a15 = {
+    'prim': [1, 2, 23],
+    'one': [3, 5],
+    'testt': [6, 7, 8],
+    'ivan': [9, 10]
+};
+
+function f15() {
+    let out = '';
+    for(key in a15) {
+        for(val in a15[key]) {
+            out += a15[val] + ' ' + `<br>`;
+        }
+    }
+    document.querySelector('.o15').innerHTML = out;
+}
+
+document.querySelector('.b15').onclick = f15;
